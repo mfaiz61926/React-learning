0e167defcab2a1e81e8a68656af1f4a2f8618f07
Mohd faiz <92942111+mfaiz61926@users.noreply.github.com>
2025-03-04 21:56:37 +0530

    Update day9useCase.js

--- day9useCase.js
+++ day9useCase.js
@@ -1,1 +1,43 @@
+// app.js
+import React, { useState } from "react";
+const App=()=>{
+    // console.log(useState())
+    const [count,setCount]=useState(0);
+    console.log(count);
+    console.log(setCount);
+    const handleClickInc=()=>{
+        setCount(count+1);
+    }
+    const handleClickDec=()=>{
+        setCount(count-1);
+    }
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleClickInc}>+</button>
+            <button onClick={handleClickDec}>-</button>
+        </div>
+    )
+}
 
+export default App;
+
+
+
+
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { Hello } from "./App";
+// import App,{Hello} from "./App";
+import App from "./App";
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+    <App/>
+    
+    </React.StrictMode>
+)
